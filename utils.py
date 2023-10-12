75ac5bf497fdbec5743bb28e75c98da2170a58bd
jamorr <55964176+jamorr@users.noreply.github.com>
2023-10-12 10:11:58 -0400

    move functions for reading data and common vis

--- utils.py
+++ utils.py
@@ -3,22 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def read_classification_dataset(number:int = 1)->tuple[pd.DataFrame, ...]:
-    train = pd.read_csv(
-        f"./classify/TrainData{number}.txt",
-        header=None, sep='\t',
-        na_values=["1.000000e+99"]
-        )
-    target = pd.read_csv(
-        f"./classify/TrainLabel{number}.txt",
-        header=None,
-        sep='\t'
-        )
-    test = pd.read_csv(
-        f"./classify/TestData{number}.txt",
-        header=None,
-        sep='\t',
-        na_values=["1.000000e+99"]
-        )
+    train = read_missing(f"./classify/TrainData{number}.txt")
+    target = read_missing(f"./classify/TrainLabel{number}.txt")
+    test = read_missing(f"./classify/TestData{number}.txt")
     return train, target, test
 
 def features_histograms_mean_std(data:pd.DataFrame)->None:
@@ -27,3 +14,11 @@ def features_histograms_mean_std(data:pd.DataFrame)->None:
     fig.set_size_inches(10,10)
     sns.histplot(stats['mean'], ax=ax1)
     sns.histplot(stats['std'], ax=ax2)
+
+def read_missing(path:str, missing:list[str]=["1.000000e+99"])->pd.DataFrame:
+    return pd.read_csv(
+        path,
+        header=None,
+        sep='\t',
+        na_values=missing
+        )
