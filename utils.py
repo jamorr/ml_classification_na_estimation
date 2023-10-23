25b0879a57153dbc9375bdd2f7efb47259126c1d
jamorr <55964176+jamorr@users.noreply.github.com>
2023-10-23 15:45:44 -0400

    reformatted utils

--- utils.py
+++ utils.py
@@ -2,23 +2,21 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def read_classification_dataset(number:int = 1)->tuple[pd.DataFrame, ...]:
+
+def read_classification_dataset(number: int = 1) -> tuple[pd.DataFrame, ...]:
     train = read_missing(f"./classify/TrainData{number}.txt")
     target = read_missing(f"./classify/TrainLabel{number}.txt")
     test = read_missing(f"./classify/TestData{number}.txt")
     return train, target, test
 
-def features_histograms_mean_std(data:pd.DataFrame)->None:
-    stats = data.describe().T
-    fig, (ax1, ax2) = plt.subplots(2,1)
-    fig.set_size_inches(10,10)
-    sns.histplot(stats['mean'], ax=ax1)
-    sns.histplot(stats['std'], ax=ax2)
 
-def read_missing(path:str, missing:list[str]=["1.000000e+99"])->pd.DataFrame:
-    return pd.read_csv(
-        path,
-        header=None,
-        sep='\t',
-        na_values=missing
-        )
+def features_histograms_mean_std(data: pd.DataFrame) -> None:
+    stats = data.describe().T
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    fig.set_size_inches(10, 10)
+    sns.histplot(stats["mean"], ax=ax1)
+    sns.histplot(stats["std"], ax=ax2)
+
+
+def read_missing(path: str, missing: list[str] = ["1.000000e+99"]) -> pd.DataFrame:
+    return pd.read_csv(path, header=None, sep="\t", na_values=missing)
