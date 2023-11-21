726876e3d34cd0ca4243e626374bd18d0663e25d
jamorr <55964176+jamorr@users.noreply.github.com>
2023-11-21 13:52:31 -0500

    added requirements

--- utils.py
+++ utils.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 
 
 def read_classification_dataset(number: int = 1) -> tuple[pd.DataFrame, ...]:
-    train = read_missing(f"./classify/TrainData{number}.txt")
+    if number in (1,3):
+        sep = "\t"
+    else:
+        sep = None
+    train = read_missing(f"./classify/TrainData{number}.txt", sep=sep)
     target = read_missing(f"./classify/TrainLabel{number}.txt")
-    test = read_missing(f"./classify/TestData{number}.txt")
+    test = read_missing(f"./classify/TestData{number}.txt", sep=sep)
     return train, target, test
 
 
@@ -18,5 +22,9 @@ def features_histograms_mean_std(data: pd.DataFrame) -> None:
     sns.histplot(stats["std"], ax=ax2)
 
 
-def read_missing(path: str, missing: list[str] = ["1.000000e+99"]) -> pd.DataFrame:
-    return pd.read_csv(path, header=None, sep="\t", na_values=missing)
+def read_missing(path: str, sep:str|None="\t", missing: list[str] = ["1.000000e+99"]) -> pd.DataFrame:
+    if sep is not None:
+        return pd.read_csv(path, header=None, sep=sep, na_values=missing)
+    else:
+        return pd.read_csv(path, header=None, delim_whitespace=True, na_values=missing)
+
