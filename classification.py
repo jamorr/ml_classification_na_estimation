dc5d9166aff55c379daf72c8fd88dcc82ff3db3c
jamorr <55964176+jamorr@users.noreply.github.com>
2023-11-26 00:37:55 -0500

    added gradient boost model

--- classification.py
+++ classification.py
@@ -5,19 +5,9 @@ import matplotlib.pyplot as plt
 from utils import read_classification_dataset, features_histograms_mean_std
 # import sklearn
 from sklearnex import patch_sklearn
-from sklearn.calibration import LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectFromModel
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import classification_report
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import cross_val_score
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer, SimpleImputer, KNNImputer
+
+from sklearn.impute import KNNImputer
 from sklearn.cluster import SpectralBiclustering, SpectralCoclustering
-from sklearn.datasets import make_biclusters
-from sklearn.metrics import consensus_score
 from collections import Counter
 # patch_sklearn()
 import math
@@ -53,15 +43,13 @@ def entropy(*args)->float:
     return out
 
 
-train, target, test = read_classification_dataset(2)
+train, target, test = read_classification_dataset(1)
 print(train)
 X = train.values
 
 imputer = KNNImputer(n_neighbors=3, weights='distance')
 data = imputer.fit_transform(X)
 print("impute done")
-axes = plt.matshow(data, cmap=plt.cm.Blues)
-plt.title("Original dataset")
 # print(target)
 unique_labels =len(np.unique(target))
 print("number of unique labels", unique_labels)
@@ -80,13 +68,3 @@ for num_clusters in range(unique_labels, unique_labels+20):
           sum([entropy(*Counter(cluster).values())*(len(cluster)/data.shape[0]) for cluster in masked_target]))
 
 assert False
-# print("consensus score: {:.3f}".format(score))
-
-fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-
-plt.matshow(fit_data, cmap=plt.cm.Blues)
-plt.title("After biclustering; rearranged to show biclusters")
-
-plt.show()
-print("done")
